cc800e0bffe8920826fa60f9a64e0eaf68622feb
satish <ravipallisatish42@gmail.com>
2026-09-01 19:17:11 +0530

    hello world

--- common_script.sh
+++ common_script.sh
@@ -19,7 +19,8 @@ check_root(){
         exit 1
     else 
         echo "you are running with root access" | tee -a $LOGS_FILE
-    fi}
+    fi
+}
 VALIDATE(){
     if [ $1 -eq 0 ]
     then
@@ -28,6 +29,49 @@ VALIDATE(){
         echo -e "$2 is $R failed $N" | tee -a $LOGS_FILE
         exit 1
     fi
+}
+app_setup(){
+    id roboshop &>$LOGS_FILE
+    if [ $? -ne 0 ]
+    then
+        useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+        VALIDATE $? "creating roboshop system user"
+    else
+        echo -e "roboshop user is $Y already present nothing to do $N"
+    fi
+    
+    mkdir -p /app &>>$LOGS_FILE
+    VALIDATE $? "creating app directory"
+
+    curl -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip &>>$LOGS_FILE
+    VALIDATE $? "downloading $app_name zip file"
+
+    rm -rf /app/* &>>$LOGS_FILE
+    cd /app
+    unzip /tmp/$app_name.zip &>>$LOGS_FILE
+    VALIDATE $? "unzipping $app_name zip file"
+}
+nodejs_setup(){
+    dnf module disable nodejs -y &>>$LOGS_FILE
+    VALIDATE $? "disabling nodejs module"
+
+    dnf module enable nodejs:20 -y &>>$LOGS_FILE
+    VALIDATE $? "enabling nodejs:20 module"
+
+    dnf install nodejs -y &>>$LOGS_FILE
+    VALIDATE $? "installing nodejs"
+
+    npm install &>>$LOGS_FILE
+    VALIDATE $? "installing nodejs dependencies"
+}
+systemd_setup(){
+
+    cp $SCRIPT_DIR/$app_name.service /etc/systemd/system/$app_name.service
+    VALIDATE $? "copying $app_name service file"
+    systemctl daemon-reload &>>$LOGS_FILE
+    systemctl enable $app_name &>>$LOGS_FILE
+    systemctl start $app_name
+    VALIDATE $? "starting $app_name service"
 }
 print_time(){
     END_TIME=$(date +%s)
