a1d5ec1097e73bca0dff2d7a41762620810c3055
satish <ravipallisatish42@gmail.com>
2026-09-06 19:56:02 +0530

    hello world

--- mongodb.sh
+++ mongodb.sh
@@ -3,7 +3,6 @@
 source ./common_script.sh
 app_name="mongodb"
 
-
 check_root
 
 cp mongo.repo /etc/yum.repos.d/mongo.repo
--- roboshop1.sh
+++ roboshop1.sh
@@ -2,9 +2,9 @@
 #!/bin/bash
 
 AMI_ID="ami-0220d79f3f480ecf5"
-SG_ID="sg-051bd3051b24472e6"
+SG_ID="sg-0d25319b661cf812b"
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalog" "user" "cart" "shipping" "payment" "dispatch" "frontend")
-ZONE_ID="Z06595273AQ0EA2LJG30D"
+ZONE_ID="Z09783873O434Q2DYUCO6"
 DOMAIN_NAME="satishdevops.shop" 
 
 
@@ -12,7 +12,7 @@ DOMAIN_NAME="satishdevops.shop"
 for instance in $@
 do 
 
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t2.micro --security-group-ids sg-051bd3051b24472e6 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-0d25319b661cf812b --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
     then  # storing ip address 
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
