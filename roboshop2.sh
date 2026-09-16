3166ceb9e42d1e50cd192f6c42b415fc37f4d9ed
satish <ravipallisatish42@gmail.com>
2026-09-16 16:48:45 +0530

    hello world

--- roboshop2.sh
+++ roboshop2.sh
@@ -12,7 +12,7 @@ do
 
     INSTANCE_ID=$(aws ec2 run-instances \
         --image-id "$AMI_ID" \
-        --instance-type t3.small \
+        --instance-type t3.micro \
         --security-group-ids "$SG_ID" \
         --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${instance}}]" \
         --query "Instances[0].InstanceId" \
